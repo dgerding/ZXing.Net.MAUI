f407e7c31df0647072b954faf5457b674c88ac5e
David Gerding <dave@gerding.us>
2024-01-16 16:16:08 -0600

    Add project files.

---
+++ ZXing.Net.MAUI/PixelBufferHolder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace ZXing.Net.Maui.Readers
+{
+	public record PixelBufferHolder
+	{
+		public Size Size { get; init; }
+
+		public
+
+#if ANDROID
+		Java.Nio.ByteBuffer
+#elif IOS || MACCATALYST
+		CoreVideo.CVPixelBuffer
+#elif WINDOWS
+		Windows.Graphics.Imaging.SoftwareBitmap
+#else
+		byte[]
+#endif
+
+		Data { get; init; }
+	}
+}
